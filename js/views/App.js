a5def486379395eb8bd5d89bd69f28e6ce3afd84
Maurice Svay <maurice@svay.com>
2016-10-13 23:47:07 +0200

    Removes unused require

--- js/views/App.js
+++ js/views/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router';
-import sketchfabSDK from '../lib/sketchfab.js';
 
 import Sidebar from '../components/Sidebar';
 import Modal from '../components/Modal';
