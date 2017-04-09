bdb3e7ffd821a01a262700c830b6f4d3e534f1e6
Maurice Svay <maurice@svay.com>
2017-04-09 20:29:02 +0200

    Replaces React by Preact

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,28 +1,34 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require( 'webpack' );
+var path = require( 'path' );
 
 module.exports = {
     devtool: 'cheap-module-source-map',
     entry: "./app.js",
     output: {
-        path: path.join(__dirname, 'docs'),
+        path: path.join( __dirname, 'docs' ),
         filename: "app.bundle.js"
     },
     module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel'
-            }
-        ]
+        loaders: [ {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel'
+        } ]
     },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {NODE_ENV: '"development"'}
-        }),
-        new webpack.EnvironmentPlugin([
+        new webpack.DefinePlugin( {
+            'process.env': {
+                NODE_ENV: '"development"'
+            }
+        } ),
+        new webpack.EnvironmentPlugin( [
             "NODE_ENV"
-        ])
-    ]
+        ] )
+    ],
+    "resolve": {
+        "alias": {
+            "react": "preact-compat",
+            "react-dom": "preact-compat"
+        }
+    }
 };
