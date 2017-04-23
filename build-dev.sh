2bf7505e9e6a9fcb60c85b3569260d28033648ea
Maurice Svay <maurice@svay.com>
2017-04-23 18:29:17 +0200

    Cleans up unused scripts

--- build-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf docs/*
-cp -R styles docs/styles
-cp -R assets docs/assets
-cp index.html docs
-cp authSuccess.html docs
-cp .nojekyll docs
-webpack --config webpack.config.dev.js --watch
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-./build.sh
-git add --all docs
-git commit -m "Updates dist build"
-git push origin master
