773e7531640c601481c754efc1c0cb2ca1cc96a3
Maurice Svay <maurice@svay.com>
2017-11-07 23:20:19 +0100

    Updates model details design

--- js/components/ModelDetail.js
+++ js/components/ModelDetail.js
@@ -13,46 +13,7 @@ class ModelDetail extends React.Component {
     render() {
         const model = this.props.model;
 
-        if (model !== null) {
-            const humanDate = relativeDate(new Date(model.publishedAt));
-            return (
-                <div className="modelDetail">
-                    <div className="modelDetail__viewer">
-                        <iframe
-                            src={model.embedUrl + "?autostart=1"}
-                            allowFullScreen
-                        />
-                    </div>
-                    <div className="modelDetail__info">
-                        <h1 className="modelDetail__title">{model.name}</h1>
-                        <span className="modelDetail__meta">
-                            <span className="modelDetail_author">by {model.user.displayName}</span> (<span className="modelDetail_date" title={model.publishedAt}>{humanDate}</span>)
-                        </span>
-                        <div className="modelDetail_actions">
-                            <LikeButton model={model} />
-                        </div>
-                        <div className="modelDetail__description">
-                            {model.description}
-                            {model.tags.map(tag => "#" + tag.name).join(", ")}
-                        </div>
-                        <ul>
-                            <li>Likes: {model.likeCount}</li>
-                            <li>Views: {model.viewCount}</li>
-                            <li>Downloads: {model.downloadCount}</li>
-                            <li>Faces: {model.faceCount}</li>
-                            <li>Vertex: {model.vertexCount}</li>
-                            <li>
-                                Categories:{" "}
-                                {model.categories
-                                    .map(category => category.name)
-                                    .join(", ")}
-                            </li>
-                            <li>Comments: {model.commentCount}</li>
-                        </ul>
-                    </div>
-                </div>
-            );
-        } else {
+        if (model === null) {
             return (
                 <div className="modelDetail">
                     <div className="modelDetail__viewer">
@@ -62,6 +23,56 @@ class ModelDetail extends React.Component {
                 </div>
             );
         }
+
+        const humanDate = relativeDate(new Date(model.publishedAt));
+        return (
+            <div className="modelDetail">
+                <div className="modelDetail__viewer">
+                    <iframe
+                        src={model.embedUrl + "?autostart=1"}
+                        allowFullScreen
+                    />
+                </div>
+                <div className="modelDetail__info">
+                    <h1 className="modelDetail__title">{model.name}</h1>
+                    <span className="modelDetail__meta">
+                        <span className="modelDetail_author">
+                            by {model.user.displayName}
+                        </span>{" "}
+                        (<span
+                            className="modelDetail_date"
+                            title={model.publishedAt}
+                        >
+                            {humanDate}
+                        </span>)
+                    </span>
+                    <div className="modelDetail_actions">
+                        <LikeButton model={model} />
+                    </div>
+                    <div className="modelDetail__description">
+                        {model.description +
+                            " " +
+                            model.tags.map(tag => "#" + tag.name).join(", ")}
+
+                        <p>
+                            Categories:{" "}
+                            {model.categories
+                                .map(category => category.name)
+                                .join(", ")}
+                        </p>
+                    </div>
+                    <ul className="modelDetail__stats">
+                        <li>⭐️ {model.likeCount} likes</li>
+                        <li>👁 {model.viewCount} views</li>
+                        <li>💬 {model.commentCount} comments</li>
+                    </ul>
+                    <ul className="modelDetail__stats">
+                        <li>Faces: {model.faceCount}</li>
+                        <li>Vertex: {model.vertexCount}</li>
+                    </ul>
+                </div>
+            </div>
+        );
     }
 }
 
