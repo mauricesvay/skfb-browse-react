a28a1f9eb812f607cd10d71072fe5aaf163aaa71
Maurice Svay <maurice@svay.com>
2017-11-07 23:32:16 +0100

    Updates likeCount when model is liked/un-liked

--- js/reducers/model.js
+++ js/reducers/model.js
@@ -4,20 +4,17 @@ import {
     FETCH_FALLBACK_SUCCESS
 } from "../actions/actions";
 
+import { ADD_LIKE, REMOVE_LIKE } from "../actions/user";
+
 function allModelsReducer(state = {}, action) {
     switch (action.type) {
         case FETCH_MODELS_SUCCESS:
-            var newState = {
-                ...state
-            };
+            var newState = { ...state };
             var uid;
             for (var i = 0, l = action.models.length; i < l; i++) {
                 uid = action.models[i].uid;
                 if (newState.hasOwnProperty(uid)) {
-                    newState[uid] = {
-                        ...newState[uid],
-                        ...action.models[i]
-                    };
+                    newState[uid] = { ...newState[uid], ...action.models[i] };
                 } else {
                     newState[uid] = action.models[i];
                 }
@@ -25,18 +22,32 @@ function allModelsReducer(state = {}, action) {
             return newState;
         case FETCH_MODEL_SUCCESS:
             var uid = action.uid;
-            var newState = {
-                ...state,
-                [uid]: action.model
-            };
+            var newState = { ...state, [uid]: action.model };
             return newState;
         case FETCH_FALLBACK_SUCCESS:
-            var newState = {
-                ...state
-            };
+            var newState = { ...state };
             var uid = action.uid;
             if (newState[uid]) {
                 newState[uid].fallback = action.fallback;
+            }
+            return newState;
+        case ADD_LIKE:
+            var uid = action.uid;
+            var newState = { ...state };
+            if (newState[uid]) {
+                newState[uid] = { ...newState[uid] };
+                newState[uid].likeCount += 1;
+            }
+            return newState;
+        case REMOVE_LIKE:
+            var uid = action.uid;
+            var newState = { ...state };
+            if (newState[uid]) {
+                newState[uid] = { ...newState[uid] };
+                newState[uid].likeCount = Math.max(
+                    0,
+                    newState[uid].likeCount - 1
+                );
             }
             return newState;
         default:
